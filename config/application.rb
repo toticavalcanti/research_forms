4ec2df4ed5c1882e03778dda242f8eec80606d25
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-23 15:04:30 -0300

    config Rack Attack in line 29 of the config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module ResearchForms
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::Attack
   end
 end
