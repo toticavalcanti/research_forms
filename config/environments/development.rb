8d2734d85c9f40dddbe2939da4c8802a692bcd59
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-23 14:45:40 -0300

    configure mailcatcher in line 40 - 42 of the config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 25 }
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
