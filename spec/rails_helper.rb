a320a2077c5e2ea7c56e0e4f439746de9ccd2bed
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-24 20:46:33 -0300

    insert devise with config.include Devise::Test::ControllerHelpers, type: :controller in line 12 of the spec/rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+config.include Devise::Test::ControllerHelpers, type: :controller
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
