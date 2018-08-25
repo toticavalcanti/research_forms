0b7e465bf3f553cc7cd7efa925d6a17b30a4612d
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-25 19:16:56 -0300

    set config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'answers/index'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'answers/show'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'answers/create'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'answers/destroy'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'questions/create'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'questions/update'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'questions/destroy'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'forms/index'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'forms/show'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'forms/update'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'forms/create'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      get 'forms/destroy'
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+      resources :forms
+      resources :questions, only: [:create, :update, :destroy]
+      resources :answers, only: [:index, :show, :create, :destroy]
     end
   end
 end
