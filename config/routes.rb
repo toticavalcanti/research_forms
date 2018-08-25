b0e17eb9c1f6939aa1cc101b43f29a5a68742967
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-25 19:06:54 -0300

    generate forms controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,36 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'forms/index'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'forms/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'forms/update'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'forms/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'forms/destroy'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
