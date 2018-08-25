cd8cd5fed3169fa2a13f99269617ac3b035be6a8
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-25 19:10:24 -0300

    generate answers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'answers/index'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'answers/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'answers/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'answers/destroy'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       get 'questions/create'
     end
   end
