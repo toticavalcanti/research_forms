6ea5a9ddbef23730a780070469b161ecf0f9cf24
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-27 15:36:33 -0300

    destroy form test in spec/requests/api/v1/api_v1_forms_spec.rb line 251 to 291

--- spec/requests/api/v1/api_v1_forms_spec.rb
+++ spec/requests/api/v1/api_v1_forms_spec.rb
@@ -183,4 +183,110 @@ RSpec.describe "Api::V1::Forms", type: :request do
       end
     end
   end
+
+   describe "PUT /forms/:friendly_id" do
+ 
+    context "With Invalid authentication headers" do
+      it_behaves_like :deny_without_authorization, :put, "/api/v1/forms/questionary"
+    end
+ 
+    context "With valid authentication headers" do
+      before do
+        @user = create(:user)
+      end
+ 
+      context "When form exists" do
+ 
+        context "And user is the owner" do
+          before do
+            @form = create(:form, user: @user)
+            @form_attributes = attributes_for(:form, id: @form.id)
+            put "/api/v1/forms/#{@form.friendly_id}", params: {form: @form_attributes}, headers: header_with_authentication(@user)
+          end
+ 
+          it "returns 200" do
+            expect_status(200)
+          end
+ 
+          it "form are updated with correct data" do
+            @form.reload
+            @form_attributes.each do |field|
+              expect(@form[field.first]).to eql(field.last)
+            end
+          end
+ 
+          it "Returned data is correct" do
+            @form_attributes.each do |field|
+              expect(json[field.first.to_s]).to eql(field.last)
+            end
+          end
+        end
+ 
+        context "And user is not the owner" do
+          before do
+            @form = create(:form)
+            @form_attributes = attributes_for(:form, id: @form.id)
+            put "/api/v1/forms/#{@form.friendly_id}", params: {form: @form_attributes}, headers: header_with_authentication(@user)
+          end
+ 
+          it "returns 403" do
+            expect_status(403)
+          end
+        end
+      end
+ 
+      context "When form dont exists" do
+        before do
+          @form_attributes = attributes_for(:form)
+        end
+ 
+        it "returns 404" do
+          delete "/api/v1/forms/#{FFaker::Lorem.word}", params: {form: @form_attributes}, headers: header_with_authentication(@user)
+          expect_status(404)
+        end
+      end
+    end
+  end
+
+  describe "DELETE /forms/:friendly_id" do
+    before do
+      @user = create(:user)
+    end
+ 
+    context "When form exists" do
+ 
+      context "And user is the owner" do
+        before do
+          @form = create(:form, user: @user)
+          delete "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+        end
+ 
+        it "returns 200" do
+          expect_status(200)
+        end
+ 
+        it "form are deleted" do
+          expect(Form.all.count).to eql(0)
+        end
+      end
+ 
+      context "And user is not the owner" do
+        before do
+          @form = create(:form)
+          delete "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+        end
+ 
+        it "returns 403" do
+          expect_status(403)
+        end
+      end
+    end
+ 
+    context "When form dont exists" do
+      it "returns 404" do
+        delete "/api/v1/forms/#{FFaker::Lorem.word}", params: {}, headers: header_with_authentication(@user)
+        expect_status(404)
+      end
+    end
+  end
 end
