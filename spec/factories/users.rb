3718b4efd120613f4ef4775491d17e31d750a6d0
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-27 11:04:31 -0300

    ident spec/factories/users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,15 +5,15 @@ FactoryGirl.define do
     end
   
   
-  factory :user do
-    uid          { FFaker::Lorem.word }
-    email        { FFaker::Internet.email }
-    name         { FFaker::Name.name }
-    nickname     { FFaker::Internet.user_name }
-    password     { FFaker::Lorem.word }
-    provider     'email'
-    confirmed_at timestamp
-    created_at   timestamp
-    updated_at   timestamp
-  end
+    factory :user do
+        uid          { FFaker::Lorem.word }
+        email        { FFaker::Internet.email }
+        name         { FFaker::Name.name }
+        nickname     { FFaker::Internet.user_name }
+        password     { FFaker::Lorem.word }
+        provider     'email'
+        confirmed_at timestamp
+        created_at   timestamp
+        updated_at   timestamp
+    end
  end
