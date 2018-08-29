08f62d13be50a117c51b7e6d5e9694bd22ff6405
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-29 13:49:14 -0300

    update tables associations in app/models/form.rb

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,9 @@
 class Form < ApplicationRecord
   belongs_to :user
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy  
   extend FriendlyId
   friendly_id :title, use: :slugged
+  validates :title, :description, :user, presence: true
+  validates :title, uniqueness: true  
 end
