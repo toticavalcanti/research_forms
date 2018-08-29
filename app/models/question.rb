f040a70a99bb8001a91998298ddb5601bcb51ec7
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-29 13:53:13 -0300

    update app/models/question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
-class Question < ApplicationRecord
+class Question &lt; ApplicationRecord
   belongs_to :form
+  has_many :questions_answers, dependent: :destroy
+  validates :title, :kind, :form, presence: true  
+  enum status: [ :short_text, :long_text, :integer, :boolean ]  
 end
