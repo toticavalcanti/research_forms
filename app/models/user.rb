d337309a52bba079cb928f57657ebfa52d7156c6
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-29 13:46:39 -0300

    insert has_many in app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User &lt; ActiveRecord::Base
+class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
