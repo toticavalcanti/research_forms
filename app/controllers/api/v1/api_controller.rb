c67e2a4ba84a614492a51038860dac67c552dcc5
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-03 17:14:20 -0300

    set catch error not found and missing parameter in app/controllers/api/v1/api_controller.rb

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,13 @@ module Api::V1
  
     class ApiController < ApplicationController
     
-      # Métodos globais
+      rescue_from(ActiveRecord::RecordNotFound) do ||
+        render(json: {message: 'Not Found'}, status: :not_found)
+      end
+    
+      rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+         render(json: {message: "Required parameter missing: #{parameter_missing_exception.param}"}, status: :bad_request)
+      end
     
     end
     
