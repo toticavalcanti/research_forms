64c85e6ccef78a530f051e281684bd31c40a2111
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-10 10:15:28 -0300

    set app/controllers/api/v1/questions_controller.rb

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,10 +1,41 @@
-class Api::V1::QuestionsController < ApplicationController
-  def create
-  end
-
+class Api::V1::QuestionsController < Api::V1::ApiController
+  before_action :authenticate_api_v1_user!
+  before_action :set_question, only: [:update, :destroy]
+  before_action :set_form  
+  before_action :allow_only_owner, only: [:create, :update, :destroy]
+ 
   def update
+    @question.update(question_params)
+    render json: @question
   end
-
+ 
+  def create
+    @question = Question.create(question_params.merge(form: @form))
+    render json: @question
+  end
+ 
   def destroy
+    @question.destroy
+    render json: {message: 'ok'}
   end
+ 
+  private
+ 
+    def set_question
+      @question = Question.find(params[:id])    
+    end
+ 
+    def set_form
+      @form = (@question)? @question.form : Form.find(params[:form_id])          
+    end
+ 
+    def allow_only_owner
+      unless current_api_v1_user == @form.user
+        render(json: {}, status: :forbidden) and return
+      end
+    end
+ 
+    def question_params
+      params.require(:question).permit(:title, :kind)          
+    end
 end
