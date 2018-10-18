3b86007ccde81630b87532c43b5a9df19b49c664
toticavalcanti <toticavalcanti@hotmail.com>
2018-10-18 14:58:04 -0300

    update app/models/answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,16 +1,16 @@
 class Answer < ApplicationRecord
   belongs_to :form
   has_many :questions_answers, dependent: :destroy
-  #accepts_nested_attributes_for :questions_answers
+  # accepts_nested_attributes_for :questions_answers
   validates :form, presence: true
-  
- 
+
+
   def self.create_with_questions_answers form, questions_answers
     answer = nil
     ActiveRecord::Base.transaction do
       answer = Answer.create(form: form)
       questions_answers.each do |qa|
-        answer.questions_answers.create(qa.permit(:question_id, :value))
+        answer.questions_answers.create(value: qa['value'], question_id: qa['question']['id'])
       end
     end
     answer
